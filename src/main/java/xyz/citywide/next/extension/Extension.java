bd608da02d6cc69493722978402c314d874f2b63
Window5 <13127531-Window5@users.noreply.gitlab.com>
2023-01-10 14:35:23 +0100

    Javadoc in Extension.java

--- src/main/java/xyz/citywide/next/extension/Extension.java
+++ src/main/java/xyz/citywide/next/extension/Extension.java
@@ -1,12 +1,9 @@
 package xyz.citywide.next.extension;
 
-import xyz.citywide.next.command.Command;
-import xyz.citywide.permissions.PermissionProvider;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import xyz.citywide.next.permission.PermissionProvider;
 
 /**
- * Normal CityStom extension that Automaticlly creates a PermissionProvider.
+ * Normal CityStom extension that Automatically creates a PermissionProvider.
  * @see xyz.citywide.citystom.Extension
  */
 public abstract class Extension extends xyz.citywide.citystom.Extension {
@@ -16,16 +13,40 @@ public abstract class Extension extends xyz.citywide.citystom.Extension {
      *
      * @see PermissionProvider
      */
+
     public static PermissionProvider provider;
 
+    /**
+     * Create a new Extension and create PermissionProvider with custom op level requirement.
+     *
+     * @param permission The permission that this extension will use.
+     * @param oplevel The custom op level requirement.
+     *
+     * @see Extension#Extension()
+     * @see Extension#Extension(String)
+     */
     public Extension(String permission, int oplevel) {
         provider = new PermissionProvider(oplevel, permission);
     }
 
+    /**
+     * Create a new Extension and create PermissionProvider.
+     *
+     * @param permission The permission that this extension will use.
+     *
+     * @see Extension#Extension()
+     * @see Extension#Extension(String) 
+     */
     public Extension(String permission) {
         provider = new PermissionProvider(permission);
     }
 
+    /**
+     * Create a new Extension and create PermissionProvider with the extension name from extension.json.
+     *
+     * @see Extension#Extension(String)
+     * @see Extension#Extension(String, int)
+     */
     public Extension() {
         provider = new PermissionProvider(getOrigin().getName());
     }
